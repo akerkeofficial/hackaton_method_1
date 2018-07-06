72ae2094f53c3363391cfea5a0d65e962967fc01
Akerke <akerkeofficial@gmail.com>
2018-07-06 23:07:09 +0600

    first

---
+++ data.php
@@ -0,0 +1,319 @@
+//////// TABLE REST КАФЕШКИ, КЛУБЫ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Неделька','cafe',3000, 4.5,	'http://www.voxpopuli.kz/img/article/120/60_main.jpg', 43.243363,76.953400,	'проспект Абая 19, Алматы');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('У Камина', 'cafe',	3500,	4,	'ukamina.jpg', 43.241883,	76.826297,		'5-й микрорайон, 21г');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Хурма','cafe',	1000,	4.2,	'hurma.jpg', 43.263642, 76.920706,'проспект Райымбека 217, Алматы');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Мармарис','cafe',	1200,	4.3	,'marmaris.jpg	', 43.227130,	76.905377,	 'Ауэзова, 179');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Ajour','hotel',	8000,	3.8 , 'ajour.jpg',	43.228997,	76.841705,		'Аксай 4, 96А');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Роза'
+,'hotel',7000
+,3.7
+,' roza.jpg ',43.232992
+,76.901670,
+
+'Таугуль 1, 52а');
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Hotel Kazakhfilm'
+,'hotel',13000
+,3.9
+,' kazakh.jpg ',43.195043
+,76.906326,
+
+'Казахфильм микрорайон,18а');
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Terra'
+,'hotel',8500
+,3.7
+,' terra.jpg ',43.232929
+,76.901228,
+
+'Джандосова, 17а');
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Туркестан'
+,'hotel',5500
+,4
+,' turkestan.jpg ',43.194460
+,76.833582,
+
+'Макатаева, 51');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values(
+'DimAl Hostel Almaty'
+,'hostel',4500
+,4.7
+,' dimhost.jpg ',43.235356
+,76.955716,
+
+'Жолдасбекова, 9/2');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('kaz-hostel'
+,'hostel',2000
+,3.8
+,' kazhostel.jpg ',43.242522
+,76.895996,
+
+'Шевченко, 190');
+
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Hostel 64'
+,'hostel',3000
+,4.8
+,' host64.jpg ',43.254483
+,76.945852,
+
+'Толе би, 64');
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('Nice' 
+,'hostel',2500
+,4.1
+,' niceh.jpg ',43.247820
+,76.947403,
+
+'Назарбаев проспект,149');
+insert into rest(name,type,cost,score,photo,longitude,latitude,address) values('sky' 
+,'hostel',4000
+,4.5
+,' skyh.jpg ',43.243482
+,76.927323
+,
+'Курмангазы, 107');
+
+
+
+
+
+
+
+
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Barcode' ,'Lounge Bar',
+4500
+,4
+,43.257513
+,76.946090
+,
+'пр-т. Назарбаева 103');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Sky bar','Lounge Bar',
+7000
+,3.9
+,43.247438
+,76.940970
+,
+'улица Желтоксан 142');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Чукотка','Lounge Bar',
+5000
+,4.3
+,43.259806,
+76.954275
+,
+'улица Гоголя 40');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Bar Duck','Lounge Bar',
+4500
+,3.5
+,43.235669,
+76.926068
+,
+'улица Пожарского 1');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Шишка Lounge','Lounge Bar',
+3000
+,4.5
+,43.262057,
+76.4993
+,
+'Жибек Жолы, 66');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Avenue Club','Club',
+3500
+,4.4
+,43.237338,
+76.902773
+,
+'ул. Мынбаева, д. 53');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Parovoz Bar','Lounge Bar',
+3500
+,4.3
+,43.236489,
+76.958903
+,
+'пр-т. Достык 132');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Relax time cafe','Lounge Bar',
+2500
+,4.0
+,43.211527,
+76.850008
+,
+'Мамыр 2-й микрорайон, 3');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Микрофон','Lounge Bar',
+5000
+,4.1
+,43.233693,
+76.879083
+,
+'Сатпаева, 90/1');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Мята Lounge','Lounge Bar',
+6000
+,4.3 , 43.218663, 76.897548, 'Гагарина проспект, 208а');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Enjoy Life Gastrolounge','Club',
+6000
+,5
+,43.239537,
+76.948967
+,
+'Назарбаева проспект, 220/3');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Bar 111','Lounge Bar',
+7000
+,4.7
+,43.233673,
+76.958314
+,
+'Самал 2-й микрорайон, 111');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('HP Lounge Bar Almaty','Lounge Bar',
+6000
+,4.7
+,43.261665,
+76.943888
+,
+'Панфилова,99');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Underground club','Club',
+5000
+,4.7
+,43.238520,
+76.914855
+,
+'Жандосова, 2Б');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Shymbulak Ski Resort','Nature',
+3000
+,4.7
+,43.128422,
+77.081639
+,
+'Горная, 640');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Medeu','Nature',
+1800
+,4.7
+,43.157442,
+77.058917
+,
+'Горная, 645');
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Кок-тобе','Nature',
+2000
+,4.6
+,43.129774,
+76.905056
+,
+'Достык проспект, 104Б');
+
+insert into rest(name,type,cost,score,longitude,latitude,address) values('Family Park','Nature',
+4000
+,4.3
+,43.226517,
+76.854694
+,
+'Улугбека проспект, 57/2');
+
+
+
+//////// TABLE ENTERTAINMENT СОБЫТИЯ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+insert into entertainment(type,name,cost,address,date) values('Концерт','Концерт Скриптонита
+',10000
+
+
+
+
+,'5/12, Алгабас-1, Алматы Арена','2018-07-09');
+insert into entertainment(type,name,cost,address,date) values('Футбол','футбольный матч между Кайрат и Астана'
+,2000
+
+
+
+
+,'Улица Абая 48, Центральный Стадион','2018-07-07');
+insert into entertainment(type,name,cost,address,date) values('Цирк','Казахский Государственный Цирк'
+,4500
+
+
+
+
+,'Улица Абая 50, Алматинский цирк','2018-07-08
+');
+insert into entertainment(type,name,cost,address,date) values('Цирк','Цирк дю Солей'
+,5000
+
+
+
+
+,'Улица Абая 50, Алматинский цирк','2018-07-09
+');
+insert into entertainment(type,name,cost,address,date) values('Цирк','Цирк дю Солей'
+,5000
+
+
+
+
+,'Улица Абая 50, Алматинский цирк','2018-07-08
+');
+insert into entertainment(type,name,cost,address,date) values('Концерт','Концерт группы “Ninety one”'
+,3000
+
+
+,'5/12 Алгабас-1, Алматы Арена','2018-07-08');
+insert into entertainment(type,name,cost,address,date) values('Концерт','Melvin и Рафаэль Мтыгян'
+,5000
+
+
+
+
+,'улица Абая 117, Музкафе music restaurant','2018-07-09
+' );
+insert into entertainment(type,name,cost,address,date) values('Цирк','Цирк Шапито Мурата Мутурганова'
+,2000
+
+
+
+
+,'улица Абая 50, Алматинский цирк','2018-07-08
+');
+insert into entertainment(type,name,cost,address,date) values('Концерт','Певица Луина на Esentai Square'
+,0
+
+
+
+
+,'Пр. Аль-Фараби 77/8','2018-07-08
+');
+insert into entertainment(type,name,cost,address,date) values('Концерт','Концерт Kental на Esentai Square'
+,0
+
+
+
+
+,'Пр. Аль-Фараби 77/8','2018-07-09
+');
+insert into entertainment(type,name,cost,address,date) values('Фестиваль','Фестиваль мороженного в молле Апорт'
+,0
+
+
+
+
+,'Ташкентский тракт 17к',DATE '2018-07-10
+');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
