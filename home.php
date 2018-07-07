d9cd834971b145cf70703ff91c5ecdd8e41687a3
Unknown <sapargaliyev9almas@gmail.com>
2018-07-08 02:16:47 +0600

    <+><changes>

--- home.php
+++ home.php
@@ -11,6 +11,17 @@ include('session.php');
 <div id="profile">
 <b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
 <b id="logout"><a href="logout.php">Log Out</a></b> 
+
+
+
+
+
+
+<form action="plan.php" method="POST">
+    <h2>Input your budget</h2>
+    <p><textarea rows="10" cols="45" name="text"></textarea></p>
+    <p><input type="submit" value="Отправить" name="money_save"></p>
+</form>
 </div>
 </body>
 </html>
