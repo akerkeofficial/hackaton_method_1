e966925e9eecd8d17291515e3a73e380e967b2f5
Akerke <akerkeofficial@gmail.com>
2018-07-08 05:04:52 +0600

    [+][find house]

---
+++ bron.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>brON</title>
+</head>
+<body>
+	<p>Спасибо! Вы успешно забронировали!!!</p>
+<!-- 	<form action="no.php">
+  	<input type="submit" value="ОК!!!">
+	</form>  -->
+
+<?php
+ include("session.php");
+
+	$idHotel=$_GET['var'];
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "method_hackaton_1";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql1="SELECT longitude,latitude FROM rest WHERE rest_id='$idHotel'";
+	$result=$conn->query($sql1);
+
+	if($result->num_rows>0){
+		while($row=$result->fetch_assoc()){
+			$long=$row['longitude'];
+			$lat=$row['latitude'];
+		}
+	}
+
+	$sql = "UPDATE users SET longitude='$long', latitude='$lat' WHERE user_email='$user'";
+
+	if ($conn->query($sql) === TRUE) {
+    	echo "Record updated successfully";
+	} else {
+    	echo "Error updating record: " . $conn->error;
+	}
+
+	$conn->close();
+?>
+
+</body>
+</html>
--- no.php
+++ no.php
@@ -1,66 +1,57 @@
+<?php
+include('session.php');
+?>
 <!DOCTYPE html>
 <html>
-<style type="text/css"></style>
-<body>
+	<style type="text/css"></style>
+	<body>
+		<div id="profile">
+			<b id="welcome">Welcome : <i><?php echo $user; ?></i></b>
+			<b id="logout"><a href="logout.php">Log Out</a></b> 
+		</div>
+		<?php
 
-<?php
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "method_hackaton_1";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "method_hackaton_1";
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		mysqli_set_charset($conn,"utf8");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-mysqli_set_charset($conn,"utf8");
+		// Check connection
+		if ($conn->connect_error) {
+		    die("Connection failed: " . $conn->connect_error);
+		} 
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+		$sql2 = "SELECT rest_id,name,address,photo FROM rest where type='hotel' OR type='hostel'";
+		$result=$conn->query($sql2);
+		//$result = mysqli_fetch_array($sql2);
+		//$image = $row2['photo'];
+		if($result->num_rows>0){
+			while($row=$result->fetch_assoc()){
+				$image[]=$row['photo'];
+				$id[]=$row['rest_id'];
+			}
+		}
 
-$sql = "SELECT rest_id,name,address FROM rest where type='hotel' OR type='hostel'";
-$result = $conn->query($sql);
-
-?>
-<?php
-
-$img = "img/accom/ajour.jpg";
-$img1 = "img/accom/terra.jpg";
-$img2 = "img/accom/dimalhostel.jpg";
-$img3 = "img/accom/hostel64.jpg";
-$img4 = "img/accom/kazakh.jpg";
-$img5 = "img/accom/kazhostel.jpeg";
-$img6 = "img/accom/nicehostel.jpg";
-$img7 = "img/accom/roza.jpg";
-$img8 = "img/accom/skyhostel.jpg";
-$img9 = "img/accom/turkestan.jpg";
-
-
-$collection = array( $img, $img1, $img2,$img3,$img4,$img5,$img6,$img7,$img8,$img9);
-
-
-    // output data of each row
-
-    while($row = $result->fetch_assoc()) {
-        foreach ( $collection as $item ) {
-
-		echo "<img style='max-width: 100px;' src='./".$item."'/>";
-		echo "<br> id: ". $row["rest_id"]. " - Name: ". $row["name"]. " " . $row["address"] . "<br>";
-
-}
-    }
-
-$conn->close();
-
-?> 
+		?>
+		<?php
+		for($i=0;$i<sizeof($id);$i++){
+			echo $id[$i]."<br>";
+			echo "<img style='max-width: 225px;' src='".$image[$i]."'>".'<br/ >';
+			echo '<a href="bron.php?var='.$id[$i].'">Забронировать!!!</a>'.'<br/ >';
+		}
+		$conn->close();
+		?> 
 
 
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!------ Include the above in your HEAD tag ---------->
 
 <body>
 
